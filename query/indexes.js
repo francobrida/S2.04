c500681d14acc381723d63062ff154d1ba016820
francobrida <francobrida@gmail.com>
2026-01-20 11:21:51 +0100

    Add index on restaurant names for query optimization

--- query/indexes.js
+++ query/indexes.js
@@ -3,3 +3,4 @@ db.restaurants.createIndex({ "location.coordinates": "2dsphere" });
 db.restaurants.createIndex({ borough: 1 });
 db.restaurants.createIndex({ grades.score: 1 });
 db.restaurants.createIndex({ grades.grade: 1 });
+db.restaurants.createIndex({ name: 1 });
